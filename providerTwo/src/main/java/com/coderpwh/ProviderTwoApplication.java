6ffca2d7eaeff23fcbc413af39a43b138db7a579
pengwenhao <pengwenhao@qnvip.cn>
2024-05-15 15:54:58 +0800

    feat:add UserController

--- providerTwo/src/main/java/com/coderpwh/ProviderTwoApplication.java
+++ providerTwo/src/main/java/com/coderpwh/ProviderTwoApplication.java
@@ -1,6 +1,11 @@
 package com.coderpwh;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(scanBasePackages ={"com.coderpwh"})
+@EnableDiscoveryClient
 public class ProviderTwoApplication {
 
 
--- providerTwo/src/main/java/com/coderpwh/com/coderpwh/controller/UserController.java
+++ providerTwo/src/main/java/com/coderpwh/com/coderpwh/controller/UserController.java
@@ -1,5 +1,20 @@
 package com.coderpwh.com.coderpwh.controller;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+
+    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    public String getUserInfo() {
+        return "This is User Service My name is Jack";
+
+    }
+
+
 }
