5ef97520475d3030e5ce8ea35a9fbfbdc1607dcf
pengwenhao <pengwenhao@aliyun.com>
2024-06-23 14:43:08 +0800

    feat:init mapper.xml

--- auth/src/main/java/com/coderpwh/AuthApplication.java
+++ auth/src/main/java/com/coderpwh/AuthApplication.java
@@ -4,9 +4,7 @@ package com.coderpwh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@EnableFeignClients(basePackageClasses = {UserFeignClient.class, MemberFeignClient.class})
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AuthApplication {
