2c439b9e88f37a949f983f44f97987b6b9984c36
pengwenhao <pengwenhao@aliyun.com>
2024-06-23 13:54:53 +0800

    feat:init SecurityConfig

---
+++ auth/src/main/java/com/coderpwh/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.coderpwh.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
+
+
+
+
+
+
+}
