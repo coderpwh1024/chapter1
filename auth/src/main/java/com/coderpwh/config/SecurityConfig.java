5199f01e6c5dd3479a3a0a36c620e86d25c1c529
pengwenhao <pengwenhao@aliyun.com>
2024-06-23 14:01:06 +0800

    fix:update securityConfig

--- auth/src/main/java/com/coderpwh/config/SecurityConfig.java
+++ auth/src/main/java/com/coderpwh/config/SecurityConfig.java
@@ -2,18 +2,54 @@ package com.coderpwh.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.naming.AuthenticationException;
+
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
+    @Autowired
+    private JwtTokenUserDetailsService userDetailsService;
+
+    /***
+     * 表单提交
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/login")
+                .permitAll()
+                .and()
+                .csrf()
+                .disable();
+    }
 
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.userDetailsService(userDetailsService);
+    }
 
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return  super.authenticationManager();
+
+    }
 
 
 }
