022cb3ac082240b7800191fb1f1d39f31475bfd9
pengwenhao <pengwenhao@aliyun.com>
2024-06-24 22:24:42 +0800

    feat:init filter

---
+++ auth/src/main/java/com/coderpwh/model/LoginVal.java
@@ -0,0 +1,16 @@
+package com.coderpwh.model;
+
+
+import lombok.Data;
+
+@Data
+public class LoginVal {
+
+
+    private  String userId;
+
+    private  String username;
+
+    private  String[] authorities;
+
+}
