ad0cbc25d66e6169d72d6283f07b1b768ee18917
pengwenhao <pengwenhao@aliyun.com>
2024-06-25 09:49:16 +0800

    fix:update filter

--- auth/src/main/java/com/coderpwh/filter/AuthenticationFilter.java
+++ auth/src/main/java/com/coderpwh/filter/AuthenticationFilter.java
@@ -1,6 +1,10 @@
 package com.coderpwh.filter;
 
 
+import cn.hutool.core.codec.Base64;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.coderpwh.model.LoginVal;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,6 +19,28 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = httpServletRequest.getHeader("token");
+
+        if(token!=null){
+            String json = Base64.decodeStr(token);
+            JSONObject jsonObject = JSONObject.parseObject(json);
+
+            String principal = jsonObject.getString("user_name");
+
+            JSONArray tempJsonArray =  jsonObject.getJSONArray("authorities");
+
+            String[]  authorites =tempJsonArray.toArray(new String[0]);
+
+            LoginVal loginVal =  new LoginVal();
+
+            loginVal.setUsername(principal);
+            loginVal.setAuthorities(authorites);
+
+            httpServletRequest.setAttribute("login_message",loginVal);
+        }
+
+        filterChain.doFilter(httpServletRequest,httpServletResponse);
 
     }
 
